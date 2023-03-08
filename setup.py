20beff947e6f6829f7ebe77a1500f66592818db1
piotr <nwg.piotr@gmail.com>
2023-03-08 02:52:59 +0100

    add langs to package_data

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={
-        "": ["resources/*"]
+        "": ["resources/*", "langs/*"]
     },
     url='https://github.com/nwg-piotr/nwg-displays',
     license='MIT',
