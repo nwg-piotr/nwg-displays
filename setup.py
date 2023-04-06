91577fd072ac990fd80e7eee74e0ceff04764a70
piotr <nwg.piotr@gmail.com>
2023-04-07 01:03:36 +0200

    bump to 0.3.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(f_name):
 
 setup(
     name='nwg-displays',
-    version='0.3.0',
+    version='0.3.1',
     description='nwg-shell output configuration utility',
     packages=find_packages(),
     include_package_data=True,
