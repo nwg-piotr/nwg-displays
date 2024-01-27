35a6d3d183eafbd6f347c1797ec657be8acd5a80
piotr <nwg.piotr@gmail.com>
2024-01-27 04:41:06 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@
 python3 setup.py install --optimize=1
 cp nwg-displays.svg /usr/share/pixmaps/
 cp nwg-displays.desktop /usr/share/applications/
+install -Dm 644 -t "/usr/share/licenses/nwg-displays" LICENSE
+install -Dm 644 -t "/usr/share/doc/nwg-displays" README.md
