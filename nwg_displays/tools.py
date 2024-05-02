7d605472029f7b7f07766465329b739a6f2adf7b
piotr <nwg.piotr@gmail.com>
2024-05-03 00:22:06 +0200

    ensure compatibility with python<3.12 #58

--- nwg_displays/tools.py
+++ nwg_displays/tools.py
@@ -29,11 +29,12 @@ def get_config_home():
 
 def hyprctl(cmd):
     # /tmp/hypr moved to $XDG_RUNTIME_DIR/hypr in #5788
-    hypr_dir = f"{os.getenv("XDG_RUNTIME_DIR")}/hypr" if os.path.isdir(
-        f"{os.getenv("XDG_RUNTIME_DIR")}/hypr") else "/tmp/hypr"
+    xdg_runtime_dir = os.getenv("XDG_RUNTIME_DIR")
+    hypr_dir = f"{xdg_runtime_dir}/hypr" if xdg_runtime_dir and os.path.isdir(
+        f"{xdg_runtime_dir}/hypr") else "/tmp/hypr"
 
     s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    s.connect(f"{hypr_dir}/{os.getenv("HYPRLAND_INSTANCE_SIGNATURE")}/.socket.sock")
+    s.connect(f"{hypr_dir}/{os.getenv('HYPRLAND_INSTANCE_SIGNATURE')}/.socket.sock")
 
     s.send(cmd.encode("utf-8"))
     output = s.recv(20480).decode('utf-8')
